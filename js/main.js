ee86698a0ad2b1477ef95674c6e456830ab4c760
Elbert McLaughlin <elbert.mclaughlin@gmail.com>
2024-10-14 17:39:59 -0400

    scroll detect cleanup

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 $(function () {
-  // scroll trigger
+  // init
+  var currentSection = '#home';
+
+  // scroll on navi tap
   $('header a').click(function (e) {
     e.preventDefault();
-    // content
     var target = $(this).attr('href');
     $('html, body').animate(
       {
@@ -10,35 +12,32 @@ $(function () {
       },
       400
     );
-    // header nav
-    if (target == '#proyectos') {
-      setTimeout(function () {
+  });
+
+  // scroll on masthead tap
+  $('section.masthead').click(function () {
+    $('html, body').animate(
+      {
+        scrollTop: $('#proyectos').offset().top - 40,
+      },
+      400
+    );
+  });
+
+  // current section
+  function setCurrentSection(section) {
+    if (section != currentSection) {
+      currentSection = section;
+      $('header li a').removeClass('on');
+      $('header li a[href="' + currentSection + '"]').addClass('on');
+      if (section == '#home') {
         $('header').animate(
           {
             scrollLeft: 0,
           },
           200
         );
-      }, 600);
-    } else {
-      $('header').animate(
-        {
-          scrollLeft: 188,
-        },
-        200
-      );
-    }
-  });
-
-  // current section
-  var currentSection = '#proyectos'; // init
-
-  function setCurrentSection(section) {
-    if (section != currentSection) {
-      currentSection = section;
-      $('header li a').removeClass('on');
-      $('header li a[href="' + currentSection + '"]').addClass('on');
-      if (section == '#proyectos') {
+      } else if (section == '#proyecto') {
         $('header').animate(
           {
             scrollLeft: 0,
@@ -50,26 +49,34 @@ $(function () {
           {
             scrollLeft: 188,
           },
-          200
+          300
         );
       }
     }
   }
 
   // scroll detect
-  var offsetTopProyectos, offsetTopNosotros, offsetTopContactanos; // init
+  var offsetBottomMasthead, offsetBottomProyecto, offsetBottomRomina; // init
 
   $(window).scroll(function () {
-    offsetTopProyectos = $('#proyectos').offset().top;
-    offsetTopNosotros = $('#nosotros').offset().top;
-    offsetTopContactanos = $('#contactanos').offset().top - 200;
+    offsetBottomMasthead =
+      $('section.masthead:first').offset().top +
+      $('section.masthead:first').outerHeight();
+    offsetBottomProyecto =
+      $('section.proyecto:first').offset().top +
+      $('section.proyecto:first').outerHeight();
+    offsetBottomRomina =
+      $('section.romina:first').offset().top +
+      $('section.romina:first').outerHeight();
 
-    if (this.scrollY < offsetTopNosotros - 200) {
-      setCurrentSection('#proyectos');
-    } else if (this.scrollY < offsetTopContactanos - 200) {
-      setCurrentSection('#nosotros');
-    } else if (this.scrollY > offsetTopContactanos - 200) {
+    if (this.scrollY > offsetBottomRomina - 200) {
       setCurrentSection('#contactanos');
+    } else if (this.scrollY > offsetBottomProyecto - 200) {
+      setCurrentSection('#nosotros');
+    } else if (this.scrollY > offsetBottomMasthead - 200) {
+      setCurrentSection('#proyectos');
+    } else {
+      setCurrentSection('#home');
     }
   });
 });
